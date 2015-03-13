e59d16715847ea7dc5e8221e0dd00d11898cf9d6
bryantfhayes <bryantfhayes@gmail.com>
2015-03-13 12:35:19 -0700

    added folder

--- project4/run.sh
+++ project4/run.sh
@@ -5,4 +5,7 @@
 ./comp test-input-4.txt
 ./comp test-input-5.txt
 ./comp test-input-6.txt
-#./comp test-input-2000.txt
+./comp test-input-2000.txt
+./comp tsp_example_1.txt
+./comp tsp_example_1.txt
+./comp tsp_example_3.txt
