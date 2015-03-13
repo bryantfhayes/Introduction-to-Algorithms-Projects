54bd010cbda786087dc76fdcc18a76c5942c7d05
Bryant Hayes <hayesbr@flip2.engr.oregonstate.edu>
2015-03-13 12:25:27 -0700

    comp tests ran

---
+++ project4/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./comp test-input-1.txt
+./comp test-input-2.txt
+./comp test-input-3.txt
+./comp test-input-4.txt
+./comp test-input-5.txt
+./comp test-input-6.txt
+#./comp test-input-2000.txt
